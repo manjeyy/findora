71fde2db3c3f2fe6098ef6a15444f46130437497
Sushantx3 <sushantsomphoru@gmail.com>
2026-05-01 20:47:28 +0545

    Adding home.jsp

--- landf/src/main/java/com/landf/features/location/LocationDAO.java
+++ landf/src/main/java/com/landf/features/location/LocationDAO.java
@@ -2,7 +2,7 @@ package com.landf.features.location;
 
 import java.util.ArrayList;
 
-public interface LocationDAO {
+public interface LocationInterface {
     public ArrayList<LocationModel> getAllLocations();
     public LocationModel getLocationById(int id);
     public LocationModel getLocationByName(String name);
